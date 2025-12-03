3b6758eb704f705f110a3a42a2236e09d743d92e
AsmitR <asmitrajaramkar7934@gmail.com>
2025-12-03 11:33:05 +0530

    Image Showing error fixing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-  },
   images: {
     domains: ['res.cloudinary.com'],
   },
